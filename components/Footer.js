d71f55f007d2b5737f2cc1fbb33d35e4deab2547
YoisSilver <azharchd1@gmail.com>
2022-03-13 14:30:07 +0530

    fixed links and a remove unused package

--- components/Footer.js
+++ components/Footer.js
@@ -13,31 +13,31 @@ export default function Footer() {
                 <div className="group1">
                     <ul className='header'>
                         <h4>Built by</h4>
-                        <li><a href='www.github.com/YoisSilver'>YoisSilver</a></li>
-                        <li><a href='www.github.com/YoisSilver'>Github</a></li>
-                        <li><a href='www.github.com/YoisSilver'>2022© </a></li>
+                        <li><a href='https://www.github.com/YoisSilver'>YoisSilver</a></li>
+                        <li><a href='https://www.github.com/YoisSilver'>Github</a></li>
+                        <li><a href='https://www.github.com/YoisSilver'>2022© </a></li>
                     </ul>
                 </div>
                 <div className="group1">
                     <ul className='header'>
                         <h4>Built using:</h4>
                         <li><a href='https://nextjs.org/'>Next.js</a></li>
-                        <li><a href="www.google.com">Mui</a></li>
+                        <li><a href="https://www.google.com">Mui</a></li>
                         <li><a href="https://sass-lang.com/">Scss</a></li>
                     </ul></div>
                 <Image src={rose} height={50} width={50} layout="fixed" alt="rose-ico" className='rose' />
                 <div className="group1">
                     <ul className='header'>
                         <h4>Hosted</h4>
-                        <li><a href='www.vercel.com'>Vercel</a></li>
-                        <li><a href='www.github.com'>Git</a></li>
+                        <li><a href='https://www.vercel.com'>Vercel</a></li>
+                        <li><a href='https://www.github.com'>Git</a></li>
                     </ul></div>
                 <div className="group4-logo">
                     <ul className='header'>
                         <h4>Demo:</h4>
-                        <li><a href='www.github.com/YoisSilver'><GitHubIcon className='icon' /></a></li>
-                        <li><a href='www.github.com/YoisSilver'><TwitterIcon className='icon' /></a></li>
-                        <li><a href='www.github.com/YoisSilver'><InterestsIcon className='icon' /></a></li>
+                        <li><a href='https://www.github.com/YoisSilver'><GitHubIcon className='icon' /></a></li>
+                        <li><a href='https://www.github.com/YoisSilver'><TwitterIcon className='icon' /></a></li>
+                        <li><a href='https://www.github.com/YoisSilver'><InterestsIcon className='icon' /></a></li>
                     </ul ></div >
             </div >
             <h6>all rights reserved™ 2022-2027</h6>
